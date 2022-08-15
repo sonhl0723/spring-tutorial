32c28300e95abb95da5799f18d306cfcc380ea65
sonhl0723 <sonhl0723@naver.com>
2022-08-15 13:42:25 +0900

    init

---
+++ src/test/java/com/springtutorial/ecommerce/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorial.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
